1b3de13aaa19509b221e42a98c3b271a1b1491ad
Unai San Segundo <unais@metropolia.fi>
2025-11-12 14:18:44 +0200

    Iteration 2 - unai

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,4 +1,4 @@
-const Service = ({ icon, title, text, details }) => {
+const Service = ({ id, icon, title, text, details, removeServices }) => {
   return (
     <article className='service'>
       <span className='service-icon'>
@@ -9,6 +9,7 @@ const Service = ({ icon, title, text, details }) => {
         <p className='service-text'>{text}</p>
         {details && <p className='service-details'>{details}</p>}
       </div>
+      <button type='button' className='btn service-btn' onClick={() => removeServices(id)}>Hide</button>
     </article>
   )
 }
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,18 +1,28 @@
 import { services } from '../data'
 import Title from './Title'
 import Service from './Service'
+import { useState } from 'react'
 
-const Services = () => {
+
+function Services() {
+  const [servicesData, setServicesData] = useState(services);
+
+  const removeServices = (id) => {
+    const newServices = servicesData.filter((service) => service.id !== id)
+    setServicesData(newServices);
+  }
+
   return (
     <section className='section services' id='services'>
       <Title title='our' subTitle='services' />
 
       <div className='section-center services-center'>
-        {services.map((service) => {
-          return <Service {...service} key={service.id} />
+        {servicesData.map((service) => {
+          return <Service {...service} key={service.id} id={service.id} removeServices={removeServices} />
         })}
       </div>
     </section>
-  )
+  );
 }
+
 export default Services
